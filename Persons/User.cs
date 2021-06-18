c44b5e32e9edf69b80ed9b76abee6ecd5e59b368
Sergey Alenichev <als.at@hotmail.com>
2021-06-18 10:34:26 +0200

    Library Class Persons has been added

---
+++ Persons/User.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Persons
+{
+    /// <summary>
+    /// This Class makes declaration for Property of User
+    /// </summary>
+    public class User
+    {
+        private string userName;
+
+        public string UserName
+        {
+            get { return userName; }
+            set { userName = value; }
+        }
+    }
+}
