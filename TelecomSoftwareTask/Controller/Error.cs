43124edf9d78cc1bc8a6be629db3b0df98c1b5ae
Sergey Alenichev <als.at@hotmail.com>
2021-06-18 10:53:58 +0200

    Methods GetData and Error are ready

---
+++ TelecomSoftwareTask/Controller/Error.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+namespace TelecomSoftwareTask.Controller
+{
+    /// <summary>
+    /// This Class shows the messages for errors
+    /// </summary>
+    public class Error
+    {
+        /// <summary>
+        /// Message for Incomming Data
+        /// </summary>
+        public void DataError()
+        {
+            MessageBox.Show("Could not to find file with Incomming Data or Data damaged", "Incomming Data Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        /// <summary>
+        /// Message for Error at serch field
+        /// </summary>
+        public void FilterError()
+        {
+            MessageBox.Show("Searching name is not exist at phone book", "Searching error", MessageBoxButton.OK, MessageBoxImage.Warning);
+        }
+    }
+}
---
+++ TelecomSoftwareTask/Data/GetData.cs
@@ -0,0 +1,50 @@
+﻿using Persons;
+using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Threading.Tasks;
+using TelecomSoftwareTask.Controller;
+
+namespace TelecomSoftwareTask.Data
+{
+    /// <summary>
+    /// Class for receiving of the data to programm
+    /// </summary>
+    public static class GetData
+    {
+        /// <summary>
+        /// Mothod for reading of file
+        /// </summary>
+        /// <returns></returns>
+        public static async Task<ObservableCollection<User>> GetIncommingsListOfUsers()
+        {
+            ObservableCollection<User> listOfUsers = new ObservableCollection<User>();
+            Error error;
+
+            try
+            {
+                //Path to the file
+                var fileLocation = AppDomain.CurrentDomain.BaseDirectory;
+                var path = System.IO.Path.Combine(fileLocation, @"Data\IncommingsData\PhoneBook_UTF8.txt");
+
+                //Reading of the all strings and adding to the ObservableCollection
+                using (StreamReader streamReader = new StreamReader(path))
+                {
+                    while (!streamReader.EndOfStream)
+                    {
+                        User user = new User();
+                        user.UserName = await streamReader.ReadLineAsync();
+                        listOfUsers.Add(user);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                error = new Error();
+                error.DataError();
+            }
+
+            return listOfUsers;
+        }
+    }
+}
