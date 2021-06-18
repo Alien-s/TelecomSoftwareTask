86bac7e118ed2b0de785c1c2499804a1648cc047
Sergey Alenichev <als.at@hotmail.com>
2021-06-18 10:56:27 +0200

    Model was added

---
+++ TelecomSoftwareTask/Model/Name.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Collections.ObjectModel;
+
+namespace TelecomSoftwareTask.Model
+{
+    /// <summary>
+    /// This Class makes declaration for Property of Name for generation of the Collection
+    /// </summary>
+    public class Name
+    {
+        public string IncommingName { get; set; }
+        public string WholeChangedName { get; set; }
+        public ObservableCollection<string> ParticalChangedName { get; set; } = new ObservableCollection<string>();
+    }
+}
